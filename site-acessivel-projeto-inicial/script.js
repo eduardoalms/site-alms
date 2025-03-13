3c2bd2a06e94ebc588ae6936dff580700e146224
eduardoalms <duduzin.alms@gmail.com>
2025-03-13 10:39:09 -0300

    Add files via upload

--- site-acessivel-projeto-inicial/script.js
+++ site-acessivel-projeto-inicial/script.js
@@ -1,11 +1,19 @@
 document.addEventListener( 'DOMContentLoaded', function?(){
-const aumentaFontBotao = document.getElementById('aumentar-fonte');
+    const aumentaFontBotao = document.getElementById('aumentar-fonte');
+    const diminuiFontBotao = document.getElementById('diminuir-fonte');
 
 let tamanhoAtualFont = 1;
 
 aumentaFontBotao.addEventListener('click', function(){
     tamanhoAtualFonte += 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+})
+
+diminuirFontBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
 })
 
 })
